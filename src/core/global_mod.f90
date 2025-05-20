64c4d5926584b66fa6dda96cb0e7084d0aad0492
Kate Mowbray <98885606+KateMowbray@users.noreply.github.com>
2025-05-20 13:25:45 +0100

    Add files via upload

---
+++ src/core/global_mod.f90
@@ -0,0 +1,278 @@
+MODULE global
+!+ contains lots of constants and global variables
+! km: Very little changed here, just added functionality for CMT2 which is set up for 2D and 2.5D fields
+  
+  USE sdf_job_info
+  USE MPI
+ 
+ IMPLICIT NONE
+  ! INCLUDE 'mpif.h'
+ 
+ SAVE 
+  
+!########################################################################## 
+ CHARACTER(Len = 4), PARAMETER	:: FMOD='CMT' ! SWITCH BETWEEN FIELDS: "l3d","l2d", "SEP","CMT", "CMT2", "test","bor", "NLFF", "MHDp"
+ INTEGER, PARAMETER		:: mysnap=0000	!  no. of ****.cfd/****.sdf file (if "l3d")
+ INTEGER, PARAMETER		:: nframes=1	! no. of frames
+ CHARACTER(Len = 40)		:: sloc='../../../../data_MHD/'
+
+!##########################################################################
+! now some stuff required to plug in lare data 
+ 
+ INTEGER, PARAMETER		:: num = KIND(1.0d0), dbl = KIND(1.D0), sp=KIND(1.0)
+ INTEGER, PARAMETER		:: NKEEPMAX = 1000001		! max no of dumps
+ INTEGER(KIND=8), PARAMETER	:: NSTPMAX  = 5E9	! max no of steps
+ INTEGER(KIND=8)		:: NSTP				! step counter
+ INTEGER, PARAMETER		:: NSTORE =50, MAXTIME=200	! how often does the RK solver output data, every NSTORE steps?
+ INTEGER 			:: ix, iy, iz,it, iix, iiy, iiz,iit, i
+ INTEGER 			:: frame 
+ 
+!JT DEBUGGING SWITCHES:
+ LOGICAL, PARAMETER		:: writervs=.TRUE., writesum=.FALSE.			! ARE WE WRITING? (ALWAYS TRUE!) 
+ LOGICAL, PARAMETER		:: JTo=.TRUE., JTo2=.FALSE., JTo3=.FALSE., JTO4=.TRUE.	! various debugging switches (2&3 output every NSTP)
+ LOGICAL, PARAMETER		:: FIELDDUMP=.FALSE.					! switch to dump the lare OR NLFFF fields to unformatted data files.
+ LOGICAL, PARAMETER		:: everystepswitch=.FALSE.				! dumps EVERY NSTP to each particle data file.
+ 
+!PARTICLE quantities: 								
+
+ REAL(num) 			:: time, tempa, tempe
+ REAL(num) 			:: Ekin,Alpha,AlphaMax,AlphaMin,dalpha, Ekinlow,Ekinhigh,T1Keep,T2Keep
+ REAL(num) 			:: T1,T2, H1,EPS, USTART, mu, USTARTKEEP, GAMMASTART,GAMMASTARTKEEP, VPARSTART, VPARSTARTKEEP
+ REAL(num), DIMENSION(3) 	:: RSTART, RSTARTKEEP
+ REAL(num), DIMENSION(3) 	:: R1,R2, tempr
+ INTEGER, DIMENSION(3) 		:: RSteps 
+ INTEGER			:: pn, nparticles
+ INTEGER			:: p_restart_no, p_stop_no
+ INTEGER 			:: EKinSteps, AlphaSteps
+ LOGICAL			:: p_restart=.FALSE., p_stop=.FALSE. 		! are we starting or stopping midway through the arrays?
+ LOGICAL			:: RANDOMISE_R, RANDOMISE_A, RANDOMISE_E	! switches for randomising position, angle and energy
+ LOGICAL			:: oob, Rlost
+
+! boundary condition choices
+ CHARACTER(Len = 5), DIMENSION(3), PARAMETER 	:: bcchoices=(/'trans','partr','fullr'/)
+ CHARACTER(Len = 5), PARAMETER 			:: xlowbc=bcchoices(1), xupbc=bcchoices(1)	!choose boundary conditions for x
+ CHARACTER(Len = 5), PARAMETER 			:: ylowbc=bcchoices(1), yupbc=bcchoices(1)	! and y
+ CHARACTER(Len = 5), PARAMETER 			:: zlowbc=bcchoices(1), zupbc=bcchoices(1) 	! and z
+ 
+ REAL(num), PARAMETER	:: tanthetathresh=0.02_num 
+
+ LOGICAL		:: maxwellEfirst
+ REAL(num), PARAMETER	:: maxwellpeaktemp= 1e6_num
+ ! Local parameters
+ REAL (num), PARAMETER  :: one = 1.0_num, zero = 0.0_num
+ 
+! CONSTANTS
+ REAL(num), PARAMETER	:: pi = 3.1415926535897932_num
+ REAL(num), PARAMETER 	:: mu0_si =  4.0e-7_num * pi
+ REAL(num), PARAMETER 	:: kb=1.380650500e-23_num
+ REAL(num), PARAMETER	:: Mp = 1.6726E-27_num, Me=9.1093826E-31_num
+ REAL(num), PARAMETER	:: Qp = 1.60217653E-19_num, Qe = -1.60217653E-19_num
+ REAL(num), PARAMETER	:: Q = Qe, M = Me					!SELECT PROTON OR ELECTRON HERE
+ REAL(num), PARAMETER	:: oneuponQ = 1.d0/Q, AQ = abs(Q), oneuponAQ= 1.d0/AQ
+ REAL(num), PARAMETER	:: MoQ=M/Q, QoM=Q/M, oM=1.0d0/M, MoAQ=M/AQ, AQoM=AQ/M	!protons
+ REAL(num), PARAMETER	:: c = 2.99792458E8_num, oneuponc=1.0_num/c   ! speed of light
+ REAL(num), PARAMETER	:: oneotwelve=1.0_num/12.0_num 
+
+! NORMALISING SCALES 
+ REAL(num), PARAMETER	:: Lscl = 1e7_num     		! 10 Mega meters (1e7)
+ REAL(num), PARAMETER	:: Bscl = 0.01_num		! 100 Gauss 	 (0.01)
+ !REAL(num), PARAMETER	:: Bscl = 1.0_num 		! 100 Gauss 	 (0.01)
+ !REAL(num), PARAMETER	:: Escl = 1e3			! 10V/cm	 (1e3)
+ !REAL(num), PARAMETER	:: Tscl = Lscl*Bscl/Escl        ! 100s	
+ REAL(num), PARAMETER	:: Tscl = 100.0_num		! 100s	
+ REAL(num), PARAMETER	:: Escl = Lscl*Bscl/Tscl	! 10V/cm	 (1e3)
+ REAL(num), PARAMETER	:: Vscl = Lscl/Tscl		! 10^7/10^2=10^5m/s=100km/s
+ REAL(num), PARAMETER	:: Ekscl = M*Vscl*Vscl		! 10^10*9e-31=9e-21joules
+ REAL(num), PARAMETER	:: Omscl=QoM*Bscl, oneuponOmscl=1.d0/Omscl		! qB/M
+ REAL(num), PARAMETER	:: jscl = Bscl/Lscl/mu0_si
+ REAL(num), PARAMETER	:: etascl = mu0_si*lscl*vscl
+ REAL(num), PARAMETER	:: myT1=0.0_num/Tscl
+ REAL(num), PARAMETER	:: pscl=bscl*bscl/mu0_si			! Lare normalisation of pressure
+ REAL(num), PARAMETER	:: rhoscl=bscl*bscl/mu0_si/vscl/vscl		! Lare normalisation of mass density
+ REAL(num), PARAMETER	:: tempscl=pscl/rhoscl/Mp*kb			! Lare normalisation of temperature
+ 
+
+ LOGICAL, PARAMETER	:: lare_norm=.TRUE.	! TRUE if Lare output IS NORMALISED, else FALSE (so normalise varibles and derivs).
+ 						! (usually true, unless someone has messed with control.f90)
+
+ REAL(num), PARAMETER	:: l3dtimenorm=1.0_num 
+
+! BIRN ET AL NORMALISATION 
+ REAL(num), PARAMETER	:: sigma=sqrt(Me/Mp), oneosigma=sqrt(Mp/Me) 				!sigma is small, oneosigma is big. 
+ REAL(num), PARAMETER	:: myepsil=Mp/abs(Qe)/Bscl/tscl, oneomyepsil=abs(Qe)*Bscl*tscl/Mp	! epsilon small
+ REAL(num), PARAMETER	:: vce=vscl/sigma
+
+
+! parameters for Guiliani et al model
+! (required by CMT module)
+ REAL(num), PARAMETER	:: d=Lscl      ! depth of monopoles
+ REAL(num), PARAMETER	:: Lv = Lscl   ! Height where stretching starts
+ REAL(num), PARAMETER	:: Bzfinal =0.00!005    !0.1426754410 
+ REAL(num), PARAMETER	:: esp=1.0  ! esponent in log transformation
+ REAL(num), PARAMETER	:: cc=0.4!(0.4/SQRT(1.05))**(2.0/3.0)!SQRT(0.4/1.05)  ! coeff in log transformation
+ REAL(num), PARAMETER	:: c1=-0.15E6   ! Magnetic Charge
+ 
+ !Separator model parameters (Wilmot-Smith & Hornig, 2011)
+ ! (required by SEP module)
+ REAL(num), PARAMETER	:: rat1=20.0_num	!r1=ratio of B1 to B0, B1=r1*B0 (in paper B1=20B0)
+ REAL(num), PARAMETER	:: rat2=1e-6_num	!r2=ratio of a to z0, a=r2*z0	(in paper a=1/2,z0=5)
+ REAL(num), PARAMETER	:: rat3=0.2_num		!r3=ratio of l to z0, l=r3*z0	(in paper ll=1,z0=5)
+
+  ! Kinematic Flux emergence model
+ REAL(num), PARAMETER	:: Lx=sqrt(2.0_num)*5.0_num, Ly=1.0_num, Lz=5.0_num
+ REAL(num), PARAMETER	:: tau=1.0_num*Tscl, tau_n=tau/Tscl
+ REAL(num), PARAMETER	:: b0=Bscl, E0=Escl		! field setup in WS&H (2011)
+ REAL(num), PARAMETER	:: L=Lscl, oL=1.0_num/L		! field setup in WS&H (2011)
+ REAL(num), PARAMETER	:: z0=5.0_num*L
+ REAL(num), PARAMETER	:: b1=rat1*b0, a=rat2*z0, ll=rat3*z0, oa=1.0_num/a, oll=1.0_num/ll 
+ REAL(num), PARAMETER	:: xc=0.0_num, yc=0.0_num, zc=0.0_num		! center of flux ring
+
+ REAL(num), PARAMETER	:: lowbthresh=1e-10_num
+ ! Lare field parameters
+ ! (required by LARE modules)
+
+ !REAL(num), DIMENSION(2), PARAMETER	:: xe=(/0.1_num,99.9_num/),ye=(/-0.1_num,99.9_num/),ze=(/-19.5_num,79.5_num/)
+ !REAL(num), DIMENSION(2), PARAMETER	:: xe=(/-0.9_num,0.9_num/),ye=(/-0.9_num,0.9_num/),ze=(/-100.00_num,100.0_num/)
+ !REAL(num), DIMENSION(2), PARAMETER	:: ze=(/-9.5_num,9.5_num/),ye=(/-1.8_num,1.8_num/),xe=(/-1.8_num,3.8_num/)
+ !REAL(num), DIMENSION(2), PARAMETER	:: ze=(/0.25_num,19.75_num/),ye=(/-7.5_num,7.5_num/),xe=(/-7.5_num,7.5_num/)	!etab-5 
+ REAL(num), DIMENSION(2), PARAMETER	:: ze=(/2.0_num,18.0_num/),ye=(/-8.5_num,8.5_num/),xe=(/-8.5_num,8.5_num/)	!etab-5
+ !REAL(num), PARAMETER			:: eta=0.001_num, jcrit=25.0_num
+ REAL(num), PARAMETER			:: eta=7.2527096E13*1e-4/Lscl/Vscl, jcrit=0.8_num, rwidth=0.05_num, etabkg=0.00001_num
+ !REAL(num), PARAMETER			:: eta=0.001_num, jcrit=0.8_num, rwidth=0.05_num, etabkg=0.00001_num		!etab-5
+ !REAL(num), PARAMETER			:: eta=0.001_num, jcrit=20.0_num, rwidth=0.5_num
+
+ CHARACTER(Len = 5), PARAMETER 	:: dloc='Data/'
+ CHARACTER(Len = 11) 		:: dlocN=dloc//'DataN/',dlocR=dloc//'DataR/'
+
+ INTEGER, PARAMETER				:: mpireal = MPI_DOUBLE_PRECISION
+ !INTEGER, PARAMETER				:: nx_global=64, ny_global=64, nz_global=64		! julie's lare3d cfd config
+ !INTEGER, PARAMETER				:: nx_global=120, ny_global=120, nz_global=480		! alan's lare3d sdf config
+ !INTEGER, PARAMETER				:: nx_global=256, ny_global=256, nz_global=512		! alan's lare3d sdf config twoloops
+ !INTEGER, PARAMETER				:: nx_global=256, ny_global=256, nz_global=256		! Steph/Duncan NLFFF resolution
+ !INTEGER, PARAMETER				:: nx_global=512, ny_global=512, nz_global=512		! alan etab-5 resolution
+ INTEGER, PARAMETER				:: nx_global=255, ny_global=255, nz_global=249		! Paolo MHD resolution
+ INTEGER, PARAMETER 				:: data_dir_max_length = 64
+ INTEGER 					:: nx, ny, nz	
+ INTEGER, DIMENSION(:), ALLOCATABLE		:: dims
+
+ REAL(num), DIMENSION(:, :, :, :), ALLOCATABLE 	:: bx, by, bz, vx, vy, vz
+ REAL(num), DIMENSION(:, :, :, :), ALLOCATABLE	:: jx, jy, jz
+ REAL(num), DIMENSION(:, :, :), ALLOCATABLE 	:: Ex, Ey, Ez
+ REAL(num), DIMENSION(:, :, :), ALLOCATABLE 	:: dbxdx,dbxdy,dbxdz,dbydx,dbydy,dbydz,dbzdx,dbzdy,dbzdz
+ REAL(num), DIMENSION(:, :, :), ALLOCATABLE 	:: dExdx,dExdy,dExdz,dEydx,dEydy,dEydz,dEzdx,dEzdy,dEzdz
+ REAL(num), DIMENSION(:), 	ALLOCATABLE 	:: myx, myy, myz, ltimes
+ REAL(num), DIMENSION(:, :, :,:), ALLOCATABLE 	:: etavar
+  
+ CHARACTER(LEN = data_dir_max_length) :: data_dir
+ CHARACTER(Len = 14)	:: mydataloc
+ CHARACTER(Len = 4)	:: filetype1='.cfd', filetype2='.sdf', filetype3='.sav'
+ CHARACTER(LEN = 20) 	:: name, class, mesh_name, mesh_class
+ 
+ !---BOURDIN DATA DEFINITIONS---! 
+ CHARACTER(Len = 8)		:: filetypeb='.bin_f77'
+ LOGICAL			:: bourdinflag, l3dflag, analyticalflag, l2dflag,FREflag, testflag, CMTflag, NLFFflag, MHDpflag
+ REAL(num), DIMENSION(2)	:: xee, yee, zee
+  
+  ! MPI data
+ INTEGER :: rank, proc_x_min, proc_x_max, proc_y_min, proc_y_max, proc_z_min, proc_z_max
+ INTEGER :: errcode, comm, tag, nproc, nprocx, nprocy, nprocz
+ INTEGER :: status(MPI_STATUS_SIZE)
+
+  ! file handling
+ INTEGER :: subtype, obstype
+ INTEGER(KIND = MPI_OFFSET_KIND) :: initialdisp
+ INTEGER :: initial
+ INTEGER :: n_zeros = 4
+ INTEGER :: output_file = 0
+  
+ LOGICAL :: gflag, pflag1=.true.
+ 
+ CHARACTER(len=7) 				:: fmt1='(I4.4)', istring 	! format descriptor
+ CHARACTER(LEN = 20+data_dir_max_length) 	:: cfdloc, sdfloc
+ 
+ ! NEW LARE3D PLUGIN INFO
+   !-- cut and pasted from "version info"--!
+  CHARACTER(LEN=*), PARAMETER :: c_code_name = 'Lare3D'
+  CHARACTER(LEN=*), PARAMETER :: c_code_name2 = 'Lare2D'
+  TYPE(jobid_type) :: jobid
+  INTEGER :: ndims, c_ndims
+  INTEGER, DIMENSION(:), ALLOCATABLE :: coordinates, n_global_min, n_global_max
+  INTEGER :: cell_subarray, cellng_subarray, cell_distribution
+  INTEGER :: node_subarray, nodeng_subarray, node_distribution
+  INTEGER :: bx_subarray, bx_distribution
+  INTEGER :: by_subarray, by_distribution
+  INTEGER :: bz_subarray, bz_distribution
+  INTEGER :: cell_xface, node_xface, node_xface1
+  INTEGER :: cell_yface, node_yface, node_yface1
+  INTEGER :: cell_zface, node_zface, node_zface1
+  INTEGER :: bx_xface, by_xface, bz_xface, bx_xface1
+  INTEGER :: bx_yface, by_yface, bz_yface, by_yface1
+  INTEGER :: bx_zface, by_zface, bz_zface, bz_zface1
+  REAL(num) :: x_min, x_max, length_x
+  REAL(num) :: y_min, y_max, length_y
+  REAL(num) :: z_min, z_max, length_z
+  INTEGER, DIMENSION(:), ALLOCATABLE :: cell_nx_mins, cell_nx_maxs
+  INTEGER, DIMENSION(:), ALLOCATABLE :: cell_ny_mins, cell_ny_maxs
+  INTEGER, DIMENSION(:), ALLOCATABLE :: cell_nz_mins, cell_nz_maxs
+  
+  INTEGER, DIMENSION(:), ALLOCATABLE	:: global_dims, local_dims
+  REAL(num), DIMENSION(:), ALLOCATABLE :: extents, extent, stagger
+ 
+!----------------------------------------------------
+ contains
+!----------------------------------------------------!
+SUBROUTINE read_param
+ Namelist/inputdata/T1,T2,H1,EPS,AlphaSteps,AlphaMin,AlphaMax,R1,R2,RSteps,EkinLow,&
+ EKinHigh,EkinSteps,p_restart, p_restart_no, p_stop, p_stop_no, &
+ RANDOMISE_R, RANDOMISE_A, RANDOMISE_E!
+
+  OPEN(20,file='newinput.dat',status='unknown')
+  ! JT: Alpha - angle between initial V & B
+  !
+  READ(20,nml=inputdata)
+  CLOSE(20)
+
+END SUBROUTINE read_param
+!----------------------------------------------------!
+SUBROUTINE init_random_seed()
+
+      INTEGER :: i, n, clock
+      INTEGER, DIMENSION(:), ALLOCATABLE :: seed
+
+      CALL RANDOM_SEED(size = n)
+      ALLOCATE(seed(n))
+
+      CALL SYSTEM_CLOCK(COUNT=clock)
+
+      seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+      CALL RANDOM_SEED(PUT = seed)
+
+      DEALLOCATE(seed)
+END SUBROUTINE
+!-----------------------------------------------;
+  FUNCTION str_cmp(str_in, str_test)
+
+    CHARACTER(*), INTENT(IN) :: str_in, str_test
+    CHARACTER(30) :: str_trim
+    LOGICAL :: str_cmp
+    INTEGER :: l
+
+    str_trim = TRIM(ADJUSTL(str_in))
+    l = LEN(str_test)
+
+    IF (l > LEN(str_in)) THEN
+      str_cmp = .FALSE.
+      RETURN
+    END IF
+
+    IF (str_trim(l+1:l+1) /= ' ') THEN
+      str_cmp = .FALSE.
+      RETURN
+    END IF
+
+    str_cmp = str_trim(1:l) == str_test
+
+  END FUNCTION str_cmp
+!----------------------------------------------------!
+
+END MODULE global
---
+++ src/core/r_main.f90
@@ -0,0 +1,450 @@
+PROGRAM reltest		! Relativistic Particle Code: Main Program (JT Dec 2015)
+!+ sets up initial field conditions, particle grid and hands over to rkdriver
+! km: only changed to add functionality for CMT2
+
+USE GLOBAL
+!USE lare_functions
+USE mpi_routines
+USE M_DRIVERR, ONLY: RKDRIVE
+USE bourdin_fields, ONLY: bour_ini, bour_fini
+USE NLFF_fields, ONLY: NLFFF_ini, NLFFF_fini
+USE MHDp_fields, ONLY: MHDp_ini, MHDp_fini
+USE M_products, ONLY: DOT, CROSS
+USE M_fields, ONLY: FIELDS
+USE gammadist_mod, ONLY: random_gamma
+
+IMPLICIT NONE
+
+ INTEGER :: NOK, NBAD
+ INTEGER :: NKEEP,time_no
+ INTEGER :: pos_no_x,pos_no_y,pos_no_z,pos_no_alpha,pos_no_ekin
+ INTEGER :: pnmax, fcount
+ INTEGER, DIMENSION(3) :: pos_no_r
+ 
+ LOGICAL :: file_exists, reset_flag
+ 
+ REAL(num), DIMENSION(3) :: gds, lbox
+ REAL(num), DIMENSION(NKEEPMAX) :: TT 
+ REAL(num), DIMENSION(NKEEPMAX,3) :: S, TOTAL
+ 
+ CHARACTER(LEN=35)	 :: finfile, sumname
+ 
+ !welcome screen
+ WRITE(*,*) "====RELATIVISTIC particle code====="
+ WRITE(*,*) "using ", FMOD, " fields module."
+ bourdinflag=.FALSE.
+ l3dflag=.FALSE.
+ l2dflag=.FALSE.
+ analyticalflag=.FALSE.
+ NLFFflag=.FALSE.
+ MHDpflag=.FALSE.
+ !reset_flag=.FALSE.
+
+ ! read in input parameters
+ CALL read_param
+    
+ ! initial setup options depend on chosen environment
+  IF ((str_cmp(FMOD, "L3D")).OR.(str_cmp(FMOD, "l3d"))) THEN
+   c_ndims=3
+   xee=xe
+   yee=ye
+   zee=ze
+   l3dflag=.TRUE.
+   CALL MPI_INIT(errcode)
+   CALL mpi_initialise      ! mpi_routines.f90
+   
+   ltimes=ltimes*l3dtimenorm
+   print*, 'loaded times:', ltimes
+   IF (((nframes.GT.1)).AND.((T1/tscl.lt.ltimes(1)).OR.(T2/tscl.gt.ltimes(nframes)))) THEN
+    PRINT*, 'FATAL ERROR!' 
+    PRINT*, '(normalised) start/end times of particle range go beyond Lare grid of times'
+    PRINT*, '-> RETHINK normalisation, ADD in more snapshots, or LIMIT orbit lifetime.'
+    STOP
+   ENDIF
+   PRINT*, '..evaluating particle array against lare grid..' 
+  ELSE IF ((str_cmp(FMOD, "L2D")).OR.(str_cmp(FMOD, "l2d"))) THEN
+   l2dflag=.TRUE.
+   c_ndims=2   
+   xee=xe
+   yee=ye
+   zee=ze
+   CALL MPI_INIT(errcode)
+   CALL mpi_initialise_2d
+   IF ((R1(3).NE.R2(3)).OR.(RSTEPS(3).GT.1)) THEN
+    PRINT*, '-FATAL ERROR-' 
+    PRINT*, 'Lare2D data requires single z position value in initial grid!!'
+    STOP
+   ENDIF
+   IF (((nframes.GT.1)).AND.((T1/tscl.lt.ltimes(1)).OR.(T2/tscl.gt.ltimes(nframes)))) THEN
+    PRINT*, '-FATAL ERROR-' 
+    PRINT*, '(normalised start/end times of particle range go beyond Lare grid of times)'
+    PRINT*, '-> RETHINK normalisation, ADD in more snapshots, or LIMIT orbit lifetime.'
+    STOP
+   ENDIF
+  ELSE IF ((str_cmp(FMOD, "SEP")).OR.(str_cmp(FMOD, "sep"))) THEN
+    analyticalflag=.TRUE.
+    PRINT*, '..evaluating particle array against analytical field bounds..'
+  ELSE IF ((str_cmp(FMOD, "CMT")).OR.(str_cmp(FMOD, "cmt"))) THEN
+      !CMT  setup?
+   CMTflag=.TRUE.
+   xee=(/(-2.0),2.0/)
+   yee=(/0.0,300.0/)
+   zee=(/(-20.0),20.0/)
+  ELSE IF ((str_cmp(FMOD, "CMT2")).OR.(str_cmp(FMOD, "cmt2"))) THEN
+      ! km: 2D and 2.5D setup
+   CMTflag=.TRUE.
+   xee=(/(-2.0),2.0/)
+   yee=(/0.0,8.0/)
+   zee=(/(-20.0),20.0/)
+  ELSE IF ((str_cmp(FMOD, "CMTO")).OR.(str_cmp(FMOD, "cmto"))) THEN
+      ! km: oscillating CMT set up
+   CMTFLAG=.TRUE.
+   xee=(/(-2.0),2.0/)
+   yee=(/0.0,8.0/)
+   zee=(/(-20.0),20.0/)
+  ELSE IF ((str_cmp(FMOD, "TEST")).OR.(str_cmp(FMOD, "test"))) THEN
+      !test setup?
+! km: added generic bounds for the test case so that it runs properly
+   xee=(/(-1.0),1.0/)
+   yee=(/(-1.0),1.0/)
+   zee=(/(-1.0),1.0/)
+   print*, '----'
+   PRINT*, '..evaluating particle array against test grid..'
+  ELSE IF ((str_cmp(FMOD, "NLFF")).OR.(str_cmp(FMOD, "nlff"))) THEN
+   NLFFflag=.TRUE.
+   CALL NLFFF_ini
+   xee=(/myx(6),myx(nx-5)/)
+   yee=(/myy(6),myy(ny-5)/)
+   zee=(/myz(6),myz(nz-5)/)     
+   print*, '----'
+   PRINT*, '..evaluating particle array against NLFF grid..' 
+  ELSE IF ((str_cmp(FMOD, "MHDp")).OR.(str_cmp(FMOD, "mhdp"))) THEN
+   MHDpflag=.TRUE.
+   CALL MHDp_ini
+   xee=(/myx(6),myx(nx-5)/)
+   yee=(/myy(6),myy(ny-5)/)
+   zee=(/myz(6),myz(nz-5)/)     
+   print*, '----'
+   PRINT*, '..evaluating particle array against Paolos MHD grid..' 
+  ELSE IF ((str_cmp(FMOD, "BOR")).OR.(str_cmp(FMOD, "bor"))) THEN
+   bourdinflag=.TRUE.
+   CALL bour_ini      ! read in data
+   xee=(/myx(6),myx(nx-5)/)
+   yee=(/myy(6),myy(ny-5)/)
+   zee=(/myz(6),myz(nz-5)/)
+   print*, '----'
+   PRINT*, '..evaluating particle array against BOURDIN grid..'
+  ELSE
+   PRINT*, "incorrect module selection, choose from:"
+   PRINT*, "['l3d','l2d','sep','CMT','test','bour', 'nlff']"
+   STOP
+  END IF
+  
+  ! chose particle range xe/ye/ze -> particles must not start outside this range!
+  IF (((R1(1)/lscl).le.xee(1)).OR.((R2(1)/lscl).ge.xee(2)))  THEN
+    WRITE(*,*) '..particles not within x extent '
+    print*, R1(1)/lscl
+    print*, xee(1) 
+    print*, R2(1)/lscl
+    print*, xee(2) 
+    gflag=.true.
+   ENDIF
+   IF (((R1(2)/lscl).le.yee(1)).OR.((R2(2)/lscl).ge.yee(2)))  THEN
+    WRITE(*,*) '..particles not within y extent '
+    gflag=.true.
+   ENDIF
+   IF (((R1(3)/lscl).le.zee(1)).OR.((R2(3)/lscl).ge.zee(2)))  THEN
+    WRITE(*,*) '..particles not within z extent '
+    gflag=.true.
+   ENDIF
+   IF (gflag) THEN
+    PRINT*, '-FATAL ERROR-'
+    WRITE(*,*) '(particle grid out of bounds)'
+    STOP
+   ELSE 
+    WRITE(*,*) 'fine!'
+   ENDIF
+   
+  DO i=1,3
+   IF (RSTEPS(i).EQ.1) THEN 	;! if rsteps=1, 1/(rsteps-1)=1/0!!
+    gds(i)=1.0_num 
+   ELSE 
+    gds(i)=1.0_num/REAL(RSTEPS(i)-1)
+   ENDIF
+  ENDDO
+  lbox=(/R2(1)-R1(1),R2(2)-R1(2),R2(3)-R1(3)/)
+
+  nparticles=RSTEPS(1)*RSTEPS(2)*RSTEPS(3)*(AlphaSteps-1)*EkinSteps
+  IF (Alphasteps == 2) THEN
+     dalpha=0
+  ELSE
+     dalpha = (AlphaMax-Alphamin)/(Alphasteps - 2.0d0) !added by S.Oskoui
+  ENDIF
+  !km: denominator adjusted to Alphasteps-2 for correct stepsize
+
+  T1Keep=T1
+  T2Keep=T2
+ 
+  IF (JTo4) open(49,file=dlocR//'finishr.tmp' ,recl=1024,status='unknown')
+  
+  PRINT*, ''
+  ! restart our calculation for certain particles within a given grid?
+  ! can use to divvy up the same grid to different CPUs.
+  IF (p_restart) THEN	! p_restart_no is the place a calculation starts again at
+   PRINT*, '--RESTARTING p_grid--'
+   pn=p_restart_no-1
+   pos_no_x = pn / (RSTEPS(2)*RSTEPS(3))  
+   pos_no_y = MOD(pn / RSTEPS(3), RSTEPS(2))
+   pos_no_z = MOD(pn,RSTEPS(3))
+   fcount=1
+  ELSE
+   PRINT*, '--starting particle grid from beginning--'   
+   pn=0
+   pos_no_x=0
+   pos_no_y=0
+   pos_no_z=0
+   fcount=0
+  ENDIF
+  
+  IF (p_stop) THEN	!p_stop_no is the particle number where we stop calculating
+   pnmax=p_stop_no-1
+  ELSE			! otherwise iterate to total number of expected particles
+   pnmax=nparticles
+  ENDIF
+  
+  IF (writesum) THEN
+   WRITE(sumname,"(A,'Rsum',i1'.dat')"),dlocR, fcount
+   inquire(file=sumname, exist=file_exists)
+   DO WHILE (file_exists)
+    print*, 'summary file called ', sumname, 'encountered! moving up one..'
+    fcount=fcount+1
+    WRITE(sumname,"(A,'Rsum',i1'.dat')"),dlocR, fcount
+    inquire(file=sumname, exist=file_exists)
+    IF (fcount.gt.9) THEN
+      PRINT*, 'TEN summary files encountered: delete or move some!'
+      EXIT 
+    ENDIF
+   ENDDO
+   print*, 'dumping start and end times, positions and energies to ', sumname
+   open(39,file=sumname,recl=1024,status='unknown')
+  ENDIF
+
+  maxwellEfirst=.TRUE.
+  DO WHILE (pos_no_x .LE. RSTEPS(1)-1)
+   DO WHILE (pos_no_y .LE. RSTEPS(2)-1)
+    1066 DO WHILE ((pos_no_z .LE. RSTEPS(3)-1).AND.(pn .LE. pnmax))
+     DO pos_no_alpha = 1, AlphaSteps-1,1 !km: first and last values have 1 taken off
+      DO pos_no_ekin = 0, EkinSteps-1,1 !km: same here to ensure alphamin and ekinmin are considered
+       pos_no_r = (/pos_no_x,pos_no_y, pos_no_z/)
+       !print*, tempr
+       IF (RANDOMISE_R) THEN
+        CALL init_random_seed()
+        CALL RANDOM_NUMBER(tempr)
+        RSTART   = R1+tempr*lbox	!randomise position in bounds set in input
+       ELSE
+        RSTART   = R1+lbox*(pos_no_r*1.0_num)*gds
+       ENDIF
+       RSTARTKEEP=RSTART     !remember where we started
+       
+       pn= pn + 1
+       !call progress(pn,nparticles) ! generate the progress bar.
+       
+       !IF (JTo4) write(49,"(I4)",advance='no'), pn	   
+	   
+       1000 format ("particle no. ",i7,"/",i7, ", R=(",ES9.2,",",ES9.2,",",ES9.2,")") 
+       print 1000, pn,nparticles, RSTART
+
+       T1=T1Keep
+       T2=T2Keep
+
+       ! calculate pitch angle
+       IF (RANDOMISE_A) THEN
+        CALL init_random_seed()
+        CALL RANDOM_NUMBER(tempa)
+        alpha = Alphamin+tempa*(Alphamax-Alphamin)
+       ELSE
+        alpha = Alphamin+dalpha*(pos_no_alpha -1)	!added by S.Oskoui
+       ENDIF
+       alpha = alpha*Pi/180.0d0				! RADEG: added by S.Oskoui
+
+       ! calculate kinetic energy - needed to define parallel and perp velocity based on alpha
+       !pos_no_ekin starts from 0, if started from 1 then (stepekin-1)
+       IF (RANDOMISE_E) THEN
+        !Ekin=EKinLow+(EKinHigh-EKinLow)*pos_no_ekin/(EkinSteps*1.0d0)*tempe   
+	Ekin= random_gamma(1.5_num, kb*maxwellpeaktemp, maxwellEfirst)
+	EKin=Ekin*6.242e18  !(convert to eV)
+	maxwellEfirst = .FALSE.
+        ELSE IF (EkinSteps == 1) THEN !added to avoid NaN if EkinSteps=1
+        Ekin=EKinLow
+        ELSE
+        Ekin=EKinLow+(EKinHigh-EKinLow)*pos_no_ekin/(EkinSteps*1.0d0-1.0)
+       !km: denominator adjusted to be Ekinsteps-1
+       ENDIF
+       
+       !PRINT*,'Normalising:'
+       RSTART=RSTART/Lscl
+       RSTARTKEEP=RSTARTKEEP/Lscl
+       T1=T1/Tscl
+       T2=T2/Tscl
+       
+       ! WARNING passing in dimensional Ekin into mu calc
+       CALL JTMUcalc(MU,USTART,GAMMASTART,Ekin,Alpha,RSTART,T1,T2, reset_flag)
+       
+       !print*, pos_no_r
+       
+       IF (reset_flag) THEN 
+        ! very small initial B detected
+        IF (RANDOMISE_R) THEN 
+	 ! if positions are random, then go back and generate a new random position	 
+	 print*, 'initial |B| is too small: trying a new position'
+	 pn=pn-1
+	 !pos_no_z=pos_no_z-1
+	 !CYCLE
+	 GO TO 1066
+	 ! remember, random variable seed based on system clock, so will take a second or two to generate a new seed!
+	ELSE   
+         ! if the position is SPECIFIED then skip this one	 
+	 print*, 'initial |B| is too small: skipping this particle'
+         CYCLE
+	ENDIF
+       ENDIF
+       
+       IF (JTo4) write(49,"(I4)",advance='no'), pn
+       
+       USTARTKEEP=USTART  
+       GAMMASTARTKEEP=GAMMASTART       
+       Ekin = Ekin*AQ/Ekscl        
+       
+       Rlost=.FALSE.
+       !Call the rk sophisticated driver, which then works out the arrays for the
+       !time steps and positions.
+       CALL RKDRIVE(RSTART,USTART,GAMMASTART,MU,T1,T2,EPS,H1,NOK,NBAD,TT,S,TOTAL)
+       
+       IF (writesum) write(39,*) Tscl*(T1), Lscl*RSTARTKEEP, oneuponAQ*(GAMMASTARTKEEP-1)*m*c*c, &
+       Tscl*(T2), Lscl*RSTART, oneuponAQ*(GAMMASTART-1)*m*c*c
+        
+       NKEEP = (NOK +NBAD)/NSTORE
+
+      ! CALL WRITE_ENDTIME(RSTART,T2,MU,VPARSTART)
+      
+      END DO
+     END DO
+      pos_no_z=pos_no_z+1
+    END DO
+    pos_no_y=pos_no_y+1
+    pos_no_z=0
+   END DO
+   pos_no_x=pos_no_x+1
+   pos_no_y=0
+  END DO
+  IF (writesum) CLOSE(39)
+  IF (JTo4) CLOSE(49)
+ !CALL MAKEFILE(time_no)
+  
+ !END DO
+ 
+ IF ((str_cmp(FMOD, "LARE")).OR.(str_cmp(FMOD, "lare"))) THEN	!forget arrays at end
+  CALL mpi_close                     ! mpi_routines.f90
+  CALL MPI_FINALIZE(errcode)
+ ENDIF
+ IF ((str_cmp(FMOD, "BOUR")).OR.(str_cmp(FMOD, "bour"))) THEN
+  CALL bour_fini      		! deallocate stuff, leave everything nice and tidy
+ ENDIF 
+ IF ((str_cmp(FMOD, "NLFF")).OR.(str_cmp(FMOD, "nlff"))) THEN
+  CALL NLFFF_fini      		
+ ENDIF 
+ IF ((str_cmp(FMOD, "MHDp")).OR.(str_cmp(FMOD, "mhdp"))) THEN
+  CALL MHDp_fini      		
+ ENDIF 
+!------------------------------------------------------------------------------!
+ Contains
+!------------------------------------------------------------------------------!
+SUBROUTINE JTMUcalc(mu,USTART,GAMMASTART, Ekin,alpha,RSTART,T1,T2, resetflag)
+
+  REAL(num), DIMENSION(3),INTENT(IN) :: RSTART
+  REAL(num), INTENT(IN) :: T1,T2, Ekin, Alpha				! 
+  REAL(num), INTENT(OUT) :: mu, gammastart, Ustart
+  REAL(num), DIMENSION(3) :: B,El,a2,a3,a4,a5,a6,a7,a8,a9,a10,ue, RT
+  !REAL(num) :: magB,vtot,vperp, vparstart!,Erest
+  REAL(num) :: modB,vtot, gamma, Etemp
+  LOGICAL, INTENT(OUT)   :: resetflag
+ 
+ resetflag=.FALSE.
+ 
+ !calculate B, E, V at this point/time:
+ CALL FIELDS(RSTART,T1,El,B,a2,a3,a4,a5,a6,a7,a8,a9,a10,T1,T2)
+
+ !calculate magnitude of B
+ modB=B(1)*B(1)+B(2)*B(2)+B(3)*B(3)
+ modB=sqrt(modB)
+ RT=RSTART
+
+ IF (modb.le.lowbthresh) THEN 
+  resetflag=.TRUE.
+ ENDIF
+
+ !print*, 'RT=', RT
+ !PRINT*, 'B=', B
+ !STOP
+ !print*, modB
+
+ !Erest = (M*c*c)*oneuponAQ
+ 
+ ! E X B drift
+ ue=cross(El,B)/dot(B,B)  !*0.5
+ Etemp=Ekin
+ 
+! print*, 'Etemp=', Ekin
+ 
+ !need to check if 1/2mUE^2 is covered by the initial KE
+ IF (Ekin.lt.0.5d0*m*vscl*vscl*dot(ue,ue)*6.242e18) THEN
+  PRINT*, 'WARNING: raising Initial KE to account for local UE drift'
+  Etemp=0.5*m*vscl*vscl*dot(ue,ue)*6.242e18
+ ENDIF
+ 
+ gamma=Etemp/m/c/c*AQ+1.0d0
+ 
+ ! vtot^2=vpar^2+vperp^2+UE^2!!, UE should NOT come out of Gamma!
+ !vtot=c/gamma*sqrt(gamma*gamma-1)			! I *think* vtot is dimensional
+ !vtot=sqrt(c*c/gamma/gamma*(gamma*gamma-1)-vscl*vscl*dot(ue,ue))
+ !vtot=sqrt(c*c-c/gamma*c/gamma-vscl*vscl*dot(ue,ue))			! THRELFALL ET AL 2015
+ vtot=sqrt(c*c*(1.0d0-1.0d0/gamma/gamma)-vscl*vscl*dot(ue,ue))
+ ! vtot=sqrt(c*c-c/gamma*c/gamma-vce*vce*dot(ue,ue))			! THRELFALL ET AL 2015
+ 
+ !print*, 'gamma ', gamma
+ !print*, 'uE^2:', vscl*vscl*dot(ue,ue)
+ !print*, 'c*c-c/gamma*c/gamma', c*c-c/gamma*c/gamma
+ !print*, 'c*c*(1.0d0-1.0d0/gamma/gamma)', c*c*(1.0d0-1.0d0/gamma/gamma)
+ !PRINT*, c*c-c/gamma*c/gamma-vscl*vscl*dot(ue,ue)
+ !print*, 'vtot ', vtot 
+ 
+ IF (vtot/=vtot) THEN
+  PRINT*, VTOT
+  PRINT*, 'Vtot NaN ENCOUNTERED'
+  STOP
+ ENDIF
+ 
+ !Ustart=(c*sqrt(gamma*gamma-1))*cos(alpha)
+ Ustart=gamma*vtot*cos(alpha)
+ 
+ 
+ !print*, vtot*cos(alpha)
+ 
+ mu=0.5_num*m*vtot*vtot*sin(alpha)*sin(alpha)*gamma*gamma/modB		
+ 
+ USTART=Ustart/vscl					! hence, non-dimensionalising..
+ GAMMASTART=gamma
+ mu=mu/m/vscl/vscl					! no bscl normalising factor - using normalised B's already!
+ !PRINT*, 'vtot=', vtot
+ !PRINT*, 'gamma=', gamma
+ !PRINT*, 'mu=', mu
+ !PRINT*, 'modB=', modB
+  !calculate mu
+  !mu=0.5_num*vperp*vperp/magB*gammastart*gammastart
+ !STOP
+ ! WRITE (19,*) RStart,T1,Ekin,Alpha, mu*magB, 0.5_num*vparstart*vparstart
+  !WRITE (19,*) vtot,vperp,vparstart,El,B,magB,mu
+
+END SUBROUTINE
+
+END PROGRAM reltest
